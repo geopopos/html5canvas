cc5f56e7e7d285fa8d11af1f5f5c9c864b506bfc
George Roros <groros2@washcoll.edu>
2017-06-21 16:43:34 -0400

    Added interactivity to circle animations: -Added event listenter to determine mouse position and increase size of circle when the mouse was near -developed a way to have random colors and sizes for each circle

--- canvas-resize/canvas.js
+++ canvas-resize/canvas.js
@@ -45,21 +45,45 @@
 
  // 	console.log(canvas);
 
+ 	var mouse = {
+ 		x: undefined,
+ 		y: undefined
+ 	}
+
+ 	var maxRadius = 40;
+ 	// var minRadius = 2;
+
+ 	var colorArray = [
+ 		'#ffaa33',
+ 		'#99ffaa',
+ 		'#00ff00',
+ 		'#4411aa',
+ 		'#ff1100',
+ 	];
+
+ 	// Interactivity
+ 	// Creating an event listener
+ 	window.addEventListener('mousemove', 
+ 		function(event){
+ 			mouse.x = event.clientX;
+ 			mouse.y = event.clientY;
+ 		}
+ 	);
+
  	// Circle Object
- 	function Circle(x, y, dx, dy, radius, dr) {
+ 	function Circle(x, y, dx, dy, radius, color) {
  		this.x = x;
  		this.y = y;
  		this.dx = dx;
  		this.dy = dy;
  		this.radius = radius;
- 		this.dr = dr;
+ 		this.minRadius = radius;
+ 		this.color = color;
 
  		this.draw = function () {
  			c.beginPath();
 			c.arc(this.x, this.y, this.radius, 0, Math.PI * 2, false);
-			c.strokeStyle = "red";
-			c.stroke();
-			c.fillStyle = "black";
+			c.fillStyle = color;
 			c.fill();
  		}
 
@@ -76,17 +100,14 @@
 			this.x += this.dx;
 			this.y += this.dy;
 
-			// //test if circles have reached max or min size
-			// if(this.radius > 60) {
-			// 	this.radius = 59;
-			// 	this.dr = -this.dr;
-			// }else if (this.radius < 0){
-			// 	this.radius = 1;
-			// 	this.dr = -this.dr;
-			// }
-
-			// //change circle size
-			// this.radius += this.dr;
+			// interactivity
+			if (mouse.x - this.x < 50 && mouse.x - this.x > -50 && mouse.y - this.y < 50 && mouse.y - this.y > -50) {
+				if(this.radius < maxRadius){
+					this.radius += 1;
+				}
+			} else if (this.radius > this.minRadius) {
+				this.radius -= 1;
+			}
 
 			this.draw();
  		}
@@ -94,14 +115,15 @@
 
  	var circleArray = [];
 
-	for(var i = 0; i < 100; i++){
-		var radius = Math.floor(Math.random() * 60);
-		var dr = 1;
+	for(var i = 0; i < 1000; i++){
+		var radius = Math.random() * 3 + 1;
+		console.log(radius);
+		var color = colorArray[Math.floor(Math.random() * colorArray.length)];
 		var x = Math.random() * (innerWidth - radius*2) + radius;
 		var y = Math.random() * (innerHeight - radius*2) + radius;
-		var dx = (Math.random() - 0.5) * 8;
-		var dy = (Math.random() - 0.5) * 8;
-		circleArray.push(new Circle(x, y, dx, dy, radius, dr));
+		var dx = (Math.random() - 0.5) * 2;
+		var dy = (Math.random() - 0.5) * 2;
+		circleArray.push(new Circle(x, y, dx, dy, radius, color));
 	}
 
 
